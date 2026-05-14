b57b336032eb9960faaebf565523afeeaa0fcd8e
Melvunx <augustine.melv@gmmail.com>
2026-05-14 14:14:17 +0200

    modify public photos | update config

--- config/apex-weapons.config.ts
+++ config/apex-weapons.config.ts
@@ -37,15 +37,38 @@ const RAW_WEAPONS = [
   { name: "RE-45", slug: "re45", type: "PISTOL" },
   { name: "P2020", slug: "p2020", type: "PISTOL" },
 
-  // SMG
+  // SMGS
   { name: "R-99", slug: "r99", type: "SMG" },
   { name: "Volt", slug: "volt", type: "SMG" },
   { name: "Alternator", slug: "alternator", type: "SMG" },
+  { name: "Prowler", slug: "prowler", type: "SMG" },
 
-  // AR
+  // ARS
   { name: "R-301", slug: "r301", type: "AR" },
   { name: "Flatline", slug: "flatline", type: "AR" },
-  // … etc
+  { name: "Havoc", slug: "havoc", type: "AR" },
+  { name: "Hemlok", slug: "hemlok", type: "AR" },
+
+  // LMGS
+  { name: "Devotion", slug: "devotion", type: "LMG" },
+  { name: "L-star", slug: "lstar", type: "LMG" },
+  { name: "Spitfire", slug: "spitfire", type: "LMG" },
+
+  // MARKSMANS
+  { name: "G7", slug: "G7", type: "MARKSMAN" },
+  { name: "Triple-Take", slug: "tripletake", type: "MARKSMAN" },
+
+  //SNIPERS
+  { name: "Charge-Rifle", slug: "chargerifle", type: "SNIPER" },
+  { name: "Kraber", slug: "kraber", type: "SNIPER" },
+  { name: "Longbow", slug: "longbow", type: "SNIPER" },
+  { name: "Sentinel", slug: "sentinel", type: "SNIPER" },
+
+  //SHOTGUNS
+  { name: "EVA-8", slug: "eva8", type: "SHOTGUN" },
+  { name: "Mastiff", slug: "mastiff", type: "SHOTGUN" },
+  { name: "Mozambique", slug: "mozambique", type: "SHOTGUN" },
+  { name: "Peacekeeper", slug: "peacekeeper", type: "SHOTGUN" },
 ] satisfies z.input<typeof WeaponSchema>[];
 
 // Validé une seule fois au démarrage
@@ -57,10 +80,13 @@ export function getWeaponsByType(type: WeaponType) {
 }
 
 export function getWeaponByName(name: string) {
-  return WEAPONS.find((w) => w.name === name);
+  return WEAPONS.find((w) => w.name === name) ?? null;
 }
 
-export function getWeaponImagePath(slug: string, type?: WeaponType) {
-  if (type) return `/weapons/${type.toLowerCase()}/${slug}.png`;
-  return `/weapons/badges/${slug}.webp`;
+export function getWeaponImagePath(type: WeaponType, slug: string) {
+  return `/apex-weapons/${type.toLowerCase()}/${slug}.png`;
+}
+
+export function getWeaponBadgePath(slug: string) {
+  return `/apex-weapons/badges/${slug}.webp`;
 }
